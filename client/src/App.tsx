b483f692b6f776c18f6c0941c9efd7d7ae4dd304
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2024-08-22 16:03:15 +0200

    Fetch data from server into client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,36 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+type Note = {
+  _id: string;
+  title: string;
+  text: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const response = await axios.get('http://localhost:4000/api/notes');
+      setNotes(response.data);
+    };
+    fetchNotes();
+  }, []);
   return (
-    <>
-      <div>
-        <h1 className="text-5xl text-red-500">Test</h1>
-      </div>
-    </>
+    <main>
+      {notes.length > 0 ? (
+        notes.map((note) => (
+          <div key={note._id}>
+            <h1>{note.title}</h1>
+            <p>{note.text}</p>
+          </div>
+        ))
+      ) : (
+        <p>No notes available</p>
+      )}
+    </main>
   );
 }
 
